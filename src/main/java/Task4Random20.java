92c77d2e2f973bd0ca1d19718114d49b921950f8
ALut19 <andreylut19@gmail.com>
2017-10-15 17:51:53 +0300

    commit 15.10

---
+++ src/main/java/Task4Random20.java
@@ -0,0 +1,23 @@
+public class Task4Random20 {
+    public static void main(String[] args) {
+
+        double b = 10.56;
+
+        int max = 0;
+        int min = 0;
+        int[] array = new int[20];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.round((Math.random() * 21) - 10);
+            System.out.println(array[i]);
+
+            if(array[i] > max){
+                max = array[i];
+            }if (array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("min " + min);
+        System.out.println("max " + max);
+    }
+}
