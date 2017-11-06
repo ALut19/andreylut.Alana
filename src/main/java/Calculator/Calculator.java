e0626def0990ff7f631b3dd57a0e46f45fdf4c8e
ALut19 <andreylut19@gmail.com>
2017-11-06 19:53:26 +0300

    Merge branch 'master' of https://github.com/ALut19/andreylut.Alana
                               # Conflicts:
                               #	src/main/java/Gift.java

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,38 +1,70 @@
 package Calculator;
 
+import java.util.Scanner;
+
 /**
  * Created by user on 27.10.2017.
  */
 public class Calculator {
+  // Scanner scan = new Scanner(System.in);
+
     double first;
     double second;
-    double result = 0;
+    double result;
     String action;
 
     public Calculator(double first, double second, String action) {
+
         this.first = first;
         this.second = second;
+        this.result = result;
         this.action = action;
     }
 
-    public Calculator() {
 
+    public double getFirst() {
+        return first;
     }
 
+    public void setFirst(double first) {
+        this.first = first;
+    }
 
-    public void setfirst(double name) {
+    public double getSecond() {
+        return second;
+    }
+
+    public void setSecond(double second) {
+        this.second = second;
+    }
+
+    public double getResult() { return result; }
+
+    public void setResult(double result) { this.result = result; }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+    /*  public void setfirst(double name) {
         this.first = name;
-    }
-
-    public void calcplus()
-    {
-        this.result = this.first + this.second;
-        System.out.println(this.result);
-    }
+    }*/
 
 
-    public int fi()
-    {
-        return 1;
+    public Calculator() {
+        if (this.action.equals("+")) {
+            this.result = this.first + this.second;
+        }
+        System.out.println(result);
+
+    /*    @Override
+        public String toString () {
+            return "first = " + first + ", second = " + second + ", result = " + result;
+        }*/
+
     }
+
 }
--- src/main/java/Calculator/WorkCalc.java
+++ src/main/java/Calculator/WorkCalc.java
@@ -1,6 +1,7 @@
 package Calculator;
 
 import Calculator.Calculator;
+import Gift.GiftParam;
 
 import java.util.Scanner;
 
@@ -12,16 +13,20 @@ public class WorkCalc {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Calculator Calcul = new Calculator(10,15,
-                "+");
-        Calculator c2 = new Calculator(input.nextDouble(),input.nextDouble(),input.next());
+
+    //    Calculator Calcul = new Calculator(10,15,"+");
+        Calculator c2 = new Calculator(input.nextDouble(), input.nextDouble(), input.next());
+      //  System.out.println(c2.toString());
 
 
    //     Calcul.setfirst(input.nextDouble());
 
-        Calcul.calcplus();
-        c2.calcplus();
+    //    Calcul.calcplus();
+
+
+
+        }
     }
 
-    }
+
 
